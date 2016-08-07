e5357ba5f8edda19e07ec7139ee6747d17ba7415
xxdeyy <xxdeyy@gmail.com>
2016-08-07 12:25:31 +0800

    sfsdf

---
+++ f2boy_py/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for f2boy_py project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "f2boy_py.settings")
+
+application = get_wsgi_application()
