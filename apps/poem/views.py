75e8f3f03857f54d974351389afc9d181144d9a8
xxdeyy <xxdeyy@gmail.com>
2016-09-11 18:19:55 +0800

    sdfs

--- apps/poem/views.py
+++ apps/poem/views.py
@@ -11,7 +11,7 @@ def detail(request, curr_poem):
     poems['mcdh'] = '面朝大海'
     poems['yzyr'] = '有朝一日'
 
-    if curr_poem == '':
+    if curr_poem not in poems:
         curr_poem = 'yzyr'
 
-    return render(request, 'poem/detail.html', {'poems': poems, 'curr_poem': curr_poem})
+    return render(request, 'poem/' + curr_poem + '.html', {'poems': poems, 'curr_poem': curr_poem})
