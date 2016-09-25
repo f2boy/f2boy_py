082cffdf18708a2dd731d1ddaf48f940fa7b2909
xxdeyy <xxdeyy@gmail.com>
2016-09-25 23:15:23 +0800

    sdfs

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from apps.message.views import wall
+from apps.message.views import add
 
 app_name = 'message'
 urlpatterns = [
     url(r'^$', wall),
+    url(r'^add/$', add, name='add'),
 ]
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def wall(request):
     return render(request, 'wall.html')
+
+
+def add(request):
+    nickname = request.POST['nickname']
+    message = request.POST['message']
+    return HttpResponse("Hello, " + nickname + ". 你的留言：" + message)
